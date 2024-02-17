6602be2daade8743121fcddb1fe0481c1db2234b
mpanczak <panczakmateusz@gmail.com>
2024-02-17 19:01:35 +0100

    thread listener

--- src/main/java/com/github/mpanczak/httpserver/HttpServer.java
+++ src/main/java/com/github/mpanczak/httpserver/HttpServer.java
@@ -2,19 +2,15 @@ package com.github.mpanczak.httpserver;
 
 import com.github.mpanczak.httpserver.config.Configuration;
 import com.github.mpanczak.httpserver.config.ConfigurationManager;
+import com.github.mpanczak.httpserver.core.ServerListenerThread;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 /**
  * Driver Class for the Http Server
  *
  */
-public class HttpServer
-{
+public class HttpServer {
     public static void main( String[] args )
     {
         System.out.println( "Server starting..." );
@@ -25,31 +21,12 @@ public class HttpServer
         System.out.println("Using Port: " + conf.getPort());
 
         try {
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
-            final String CRLF = "\n\r"; // 13, 10
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
-                            "Content-Length: " + html.getBytes().length + CRLF + // HEADER
-                            CRLF +
-                            html +
-                            CRLF + CRLF;
-
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
-
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
             e.printStackTrace();
+            // TODO handle later
         }
+
     }
 }
