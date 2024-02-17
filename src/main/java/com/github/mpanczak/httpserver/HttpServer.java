6f45f666d98e45c20bd1bacc57351b29748dca38
mpanczak <panczakmateusz@gmail.com>
2024-02-17 18:33:57 +0100

    server socket, first run

--- src/main/java/com/github/mpanczak/httpserver/HttpServer.java
+++ src/main/java/com/github/mpanczak/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.github.mpanczak.httpserver;
 import com.github.mpanczak.httpserver.config.Configuration;
 import com.github.mpanczak.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Driver Class for the Http Server
  *
@@ -14,10 +20,36 @@ public class HttpServer
         System.out.println( "Server starting..." );
 
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
-
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
 
         System.out.println("Using Port: " + conf.getPort());
-        System.out.println("Using WebRoot: " + conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Simple Java HTTP Server</title></head><body><h1>This page was served using my Simple Java HTTP Server</h1></body></html>";
+            final String CRLF = "\n\r"; // 13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                            "Content-Length: " + html.getBytes().length + CRLF + // HEADER
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
