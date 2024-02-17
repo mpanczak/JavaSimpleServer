1903dd83d7ead527a0b0ac8f2c4048669b79997d
mpanczak <panczakmateusz@gmail.com>
2024-02-17 17:47:14 +0100

    configuration loading

--- src/main/java/com/github/mpanczak/httpserver/HttpServer.java
+++ src/main/java/com/github/mpanczak/httpserver/HttpServer.java
@@ -1,5 +1,8 @@
 package com.github.mpanczak.httpserver;
 
+import com.github.mpanczak.httpserver.config.Configuration;
+import com.github.mpanczak.httpserver.config.ConfigurationManager;
+
 /**
  * Driver Class for the Http Server
  *
@@ -9,5 +12,12 @@ public class HttpServer
     public static void main( String[] args )
     {
         System.out.println( "Server starting..." );
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("Using Port: " + conf.getPort());
+        System.out.println("Using WebRoot: " + conf.getWebroot());
     }
 }
